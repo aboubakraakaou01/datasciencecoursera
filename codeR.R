098b93e33a13f41c09d613a062ca5bbb6c6dc341
aboubakr aakaou <75222307+aboubakraakaou01@users.noreply.github.com>
2020-12-13 00:38:27 +0100

    Update codeR.R

--- codeR.R
+++ codeR.R
@@ -1,4 +1,3 @@
-
 #hello datasciencecoursera
 
 #library files
@@ -47,7 +46,7 @@ model <- lm(formula=user_id ~ ., data=trainSet)
 summary(model)
 
 #make a residuals plot
-mresiduals <- as.data.frame(residuals(model))
+modelResiduals <- as.data.frame(residuals(model))
 ggplot(modelResiduals, aes(residuals(model))) +
   geom_histogram(fill='deepskyblue', color='white')
 
@@ -56,7 +55,7 @@ pred <- predict(model,testSet)
 
 #evaluation the model
 modEv <-  cbind(testSet$user_id,pred)
-colnames(modelEv) <- c('actual', 'prediction')
+colnames(modEv) <- c('actual', 'prediction')
 modEv <- as.data.frame(modEv)
 
 #Logistic Regression..................................
@@ -67,52 +66,74 @@ sapply(df1,function(x) length(unique(x)))
 
 missmap(df1, main = "missing values vs observed our data")
 
-data <- subset(df1,select=c(1,2,5,6,7,8,15))
-data
+data <- subset(df1,select=c(7,8,18))
+
 #taking care of the missing values
-
-data$acount_lang[is.na(data$account_lang)] <- mean(data$account_lang,na.rm=T)
-
-is.factor(data$account_lang)
+data$account_lang[is.na(data$account_lang)] <- mean(data$account_lang,na.rm=T)
+is.factor(data$user_id)
 
 #model fitting
 train <- data[1:200,]
-test <- data[201:600,]
+test <- data[201:400,]
 
-model <- glm(user_id ~., family=binomial(link="logit",data=train))
 
+model <- glm(user_id ~.,family=binomial(link='logit'),data=train)
 summary(model)
 
-#analyze the table of deviance
-anova(model,test="chisq")
-#predictive ability of the model
-fitted.results <- predict(model,newdata=subset(test,select=c(2,3,4)),type='response')
+#running the model to analyze the table of deviance
+anova(model,test="Chisq")
+
+#assessing the predictive 
+fitted.results <- predict(model,newdata=subset(test,select=c(7,8,18)),type='response')
 fitted.results <- ifelse(fitted.results > 0.5,1,0)
 misClasificError <- mean(fitted.results != test$user_id)
-print(paste('Accuracy',1-misClasificError))
+print(paste('Accuracy of model',1-misClasificError))
 
-#Random Forest with R..................................
-       
-install.packages("randamForest")
+
+#Random Forest with R.............................................
+
+install.packages("randomForest")
 library(randomForest)
-       
-# rows*columns
-dim(df1)
-summary(df1)
-df1[df1 =="?"] <- NA
+
+df1[ df1 == "?"] <- NA
+colSums(is.na(df1))
 
 sample = sample.split(data$user_id, SplitRatio = .80)
 train = subset(data, sample == TRUE)
 test  = subset(data, sample == FALSE)
-  
 dim(train)
 dim(test)
 
-#intialize an instance of the random Forest class
-rf <- randomForest( user_id ~., data=train)
-       
-#prediction 
-pred= predict(rf, newdata=test[18])
-cm=table(test[18],pred)
-       
-#
+#intialize an instance of the randomForest class
+rf <- randomForest(user_id ~ ., data=train)
+
+#prediction
+pred=predict(rf,newdata=test[18])
+
+cm=table(test (,18),pred)
+
+#K-NN with R.....................................
+
+#normalization numeric data 
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x))) }
+
+df_1 <- as.data.frame(lapply(df1[2:9], normalize))
+
+#creating training and test data set
+df_train <- df_1[1:200,]
+df_test <- df_1[201:400,]
+
+df_train_labels <- prc[1:200, 1]
+df_test_labels <- prc[201:400, 1]
+#training a model on data
+install.packages("class")
+library(class)
+
+df_test_pred <- knn(train=df_train,test=df_test,cl=df_train_labels,k=10)
+
+#evaluate the model performance
+ install.packages("gmodels")
+ library(gmodels)
+ 
+ CrossTable(x=df_test_labels,y=df_test_pred,prop.chisq=FALSE)
